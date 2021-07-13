37597563efc33bf5c4df4e26df0f970ba91b2ce5
Romina Mahinpei <mahinpei@student.ubc.ca>
2021-07-12 19:31:56 -0700

    Update SliderData.js

--- src/components/SliderData.js
+++ src/components/SliderData.js
@@ -1,5 +1,5 @@
 export const SliderData = [
-    {image: 'images/stephden.jpg', caption: 'First YouTube Channel: Steph Gordon & Dennis Mathu'},
+    {image: 'images/stephden.jpeg', caption: 'First YouTube Channel: Steph Gordon & Dennis Mathu'},
     {image: 'images/griffin.png', caption: 'Second YouTube Channel: Griffin Milks'},
     {image: 'images/brandon.png', caption: 'Third YouTube Channel: Brandon Beavis'},   
 ]

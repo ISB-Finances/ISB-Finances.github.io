f3a51da77d6aae12daa719ce0d0a4f29da3bd940
ssadh42 <63157704+ssadh42@users.noreply.github.com>
2021-08-27 03:55:59 +0530

    about data

---
+++ about.js
@@ -0,0 +1,41 @@
+
+export const ABOUT = 
+[
+        {
+            id: 0,
+            name: 'Crystal Yim',
+            image: 'images/download.jpg',
+            category: '2nd Year B.Sc. Computer Science & Math',
+            description: 'Financial literacy is an important and empowering topic that often goes unrecognized. When I started university, I personally did not have much financial knowledge and noticed the same trend among other young adults. In order to do something about this issue, I decided to create ISB Finances with the talented developers in my team. I mainly worked on the Home page, Resources page, and partially on the Glossary page and learned a lot about web development via React and group collaboration via GitHub & Git.',
+
+        },{
+            id: 1,
+            name: 'Kiera Moore',
+            image: 'images/download.jpg',
+            category: '2nd Year B.Sc. Computer Science & Math',
+            description: 'Financial literacy is an important and empowering topic that often goes unrecognized. When I started university, I personally did not have much financial knowledge and noticed the same trend among other young adults. In order to do something about this issue, I decided to create ISB Finances with the talented developers in my team. I mainly worked on the Home page, Resources page, and partially on the Glossary page and learned a lot about web development via React and group collaboration via GitHub & Git.',
+            
+        },{
+            id: 2,
+            name: 'Romina Mahinpei',
+            image: 'images/download.jpg',
+            category: '2nd Year B.Sc. Computer Science & Math',
+            description: 'Financial literacy is an important and empowering topic that often goes unrecognized. When I started university, I personally did not have much financial knowledge and noticed the same trend among other young adults. In order to do something about this issue, I decided to create ISB Finances with the talented developers in my team. I mainly worked on the Home page, Resources page, and partially on the Glossary page and learned a lot about web development via React and group collaboration via GitHub & Git.',
+           
+        },{
+            id: 3,
+            name: 'Sarah Sadh',
+            image: 'images/download.jpg',
+            category: '3rd Year B.A. Cognitive Systems',
+            description: 'I wanted to build on my skill set and work on something interesting and challenging that would also be useful for others. I am also interested in finance and investing and realised that due to the vast amount of info there is its difficult to know where to begin so this could be a starting point for a students interested in finance as well.I was a complete beginner learning how to code an actual project for the first time. I learned the basics from how to pull and push code from github, how to plan, organise and execute an idea with a team remotely and how to make a basic react.js app and the basics of javascript, html and css as well. I worked on the dropdown navigation bar as well as the about us section',
+             
+            
+        },{
+            id: 4,
+            name: 'Tabreek Somani',
+            image: 'images/download.jpg',
+            category: '2nd Year B.Sc. Computer Science & Math',
+            description: 'Financial literacy is an important and empowering topic that often goes unrecognized. When I started university, I personally did not have much financial knowledge and noticed the same trend among other young adults. In order to do something about this issue, I decided to create ISB Finances with the talented developers in my team. I mainly worked on the Home page, Resources page, and partially on the Glossary page and learned a lot about web development via React and group collaboration via GitHub & Git.',
+            
+        }
+    ];
